89278f9d1793839879f394d7dbc5377dbb182b29
JIYEON <110653050+Imflower0610@users.noreply.github.com>
2022-10-07 16:04:40 +0900

    오늘 수업내용 업로드

---
+++ 오전/JavaScript/1004~1007/1007/func1.js
@@ -0,0 +1,97 @@
+
+
+/*
+1. window.onload에서 전역으로 3개의 정수값 배열로 저장
+2. 3개의 정수값은 중복없음( 범위 : 1 ~ 9 )
+3. button 클릭하면 number_input함수 실행
+4. number_input함수에서 세개의 input값 가져오기
+5. number_input함수에서 number_check함수 실행
+6. number_check함수에서 input값 3개 전달
+7. number_check함수에서 input 값과 배열과 비교하기
+8. print함수에서 div#result에 일치하는 값이 몇개인지 출력
+*/
+
+// window.onload = function () {
+// var a = new Array(3);
+// for(i=0; i<3; i++){
+//     for(j=1; j<=3; j++){
+//    if(a[i]==a[j])
+//    a[i]= Math.floor(Math.random()*9)+1;}
+// }
+// var bt = document.getElementById("send");
+// bt.addEventListener("click", number_input);
+
+// function number_input(){
+//     var num1 = document.getElementById("num1").value;
+//     num1 = parseInt(num1);
+//     var num2 = document.getElementById("num2").value;
+//     var num3 = document.getElementById("num3").value;
+//     number_check(num1, num2, num3);
+// }
+
+// function number_check(){
+//     var num_list = new Array();
+//     num_list[0]= num1;
+//     num_list[1]= num2;
+//     num_list[2]= num3;console.log(num_list);
+// }
+
+// }
+
+// 정답
+var play = new Array();
+window.onload = function(){
+    for(var i=0; i<3;){
+        var tmp = Math.floor(Math.random()*9)+1;
+        if(play.indexOf(tmp)==-1){
+            play.push(tmp);
+            i++;
+        }
+    }
+    var bt = document.querySelector("#send");
+    bt.addEventListener("click", number_input);
+};
+function number_input(){
+var user = new Array();
+var num = document.getElementsByClassName("num");
+user.push(parseInt(num[0].value));
+user.push(parseInt(num[1].value));
+user.push(parseInt(num[2].value));` `
+for(var i in num)num[i].value="";
+num[0].focus();
+if(input_check(user)){
+    alert("잘못된 숫자 입력입니다. 중복없이 1~9 중에서 입력하세요")
+    return;
+}
+number_check(user);
+}
+
+function number_check(u){
+   var strike = 0, ball =0;
+    if(u[0] == play[0]) strike++;
+    if(u[1] == play[1]) strike++;
+    if(u[2] == play[2]) strike++;
+    if(u[0] == play[1] || u[0] == play[2]) ball++;
+    if(u[1] == play[0] || u[1] == play[2]) ball++;
+    if(u[2] == play[0] || u[2] == play[1]) ball++;
+    print(strike,ball);
+}
+
+function print(strike, ball){
+    var res = document.querySelector("#result");
+    var div = document.createElement("div");
+    var out = 3-strike-ball;
+    div.appendChild(document.createTextNode( strike + "스트라이크" + ball +"볼" +out +"아웃"));
+    res.prepend(div);
+}
+
+function input_check(user){
+    for(var i in user){
+        if(user[i]<1 || user[i]>9)
+        return true;
+    }
+    if(user[0] == user[1]|| user[0]== user[2] || user[1] == user[2])
+    return true;
+
+    return false;
+}
