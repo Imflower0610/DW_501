3051db379be8c761014abf4b5f4a6036da43f752
JIYEON <110653050+Imflower0610@users.noreply.github.com>
2022-12-01 17:35:05 +0900

    fix : 자바 update 수정

--- 오후/Java/Am_study3/src/command/main_cmd.java
+++ 오후/Java/Am_study3/src/command/main_cmd.java
@@ -1,9 +1,9 @@
 package command;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
+import java.util.TreeSet;
 
 import DAO.venture_DAO;
 import VO.venture;
@@ -13,6 +13,56 @@ public class main_cmd {
 
 	public static void main(String[] args) {
 		List<venture> list = vdao.Select_all();
+		
+		TreeSet<venture> sw = new TreeSet<>();
+		TreeSet<venture> build = new TreeSet<>();
+		
+		Iterator<venture> it = list.iterator();
+		while(it.hasNext()) {
+			venture temp = it.next();
+			if(temp.getCategory().contains("정보처리"))
+				sw.add(temp);
+			if(temp.getCategory().contains("건설"))
+				build.add(temp);
+				
+		}
+		System.out.println(sw);
+	
+		String a = "hot";
+		String b = "apple";
+		System.out.println(a.compareTo(b));// a와 b의 사전적 순서의 차이
+		System.out.println(b.compareTo(a));//java에서 정렬은 -여야된다(= 정렬은 기본 오름차순이니까!)
+		
+		//treeset 메서드(검색)
+		//floor(객체) - 지정된 객체와 같은 객체를 찾아서 반환하거나, 없으면 작은 값 중에서 가까운 객체반환
+		//headSet(객체) - 지정된 객체보다 작은 객체 모두 반환
+		//tailSet(객체) - 지정된 객체보다 큰 객체 모두 반환
+		//higher(객체) - 지정된 객체보다 큰 값으 ㄴ가진 객체 중에서 가장 가까운 객체, 없으면 null
+		//lower(객체) - 지정된 객체보다 작은 값을 가진 객체 중에서 가장 가까운 객체, 없으면 null
+		//subSet(객체1, 객체2) - 범위 검색, 객체1부터 객체2 전까지 객체들 반환
+		
+		List<venture> sw_list = new ArrayList<>(sw);
+		System.out.println(sw.floor(sw_list.get(5)));
+		
+		System.out.println(sw.headSet(sw_list.get(3)));
+		System.out.println(sw.lower(sw_list.get(10)));
+		
+		System.out.println(sw.subSet(sw_list.get(2), sw_list.get(4)));
+		
+		venture srh = new venture();
+//		srh.setCompany("고");
+		srh.setCompany("(주)골나");
+		sw.add(srh);
+		System.out.println(sw.headSet(srh));
+		//TreeSet 검색방법 : 이진트리 안에 저장되어 있는 값 기준으로 검색
+		// 이진트리 안에 저장되었는 값은 객체의 주소 값이 저장되어 있다.
+		// 검색시에 저장되어있는 순서에 의해서 결과 값이 나온다
+		// 그러므로 위와 같이 검색하고자 하는 회사이름을 따로 입력해서 객체를 생성하여 
+		// 검색에 사용하는 방식은 트레셋 안에 없는 값이기 때문데 검색이 이뤄지지 않는다.
+	}
+}
+		
+/*		
 //		for (venture i : v) {
 //			System.out.println(i.getNum() + i.getCompany() + i.getAddr() + i.getCategory() + i.getBusiness_name()
 //					+ i.getProduct());
@@ -38,6 +88,84 @@ public class main_cmd {
 				build.add(data);
 			}
 	System.out.println(sw.size());
+	
+	venture a = new venture(list.get(0).getNum(),list.get(0).getCompany(),
+			list.get(0).getAddr(), list.get(0).getCategory(),
+			list.get(0).getBusiness_name(),list.get(0).getProduct());
+	
+	venture b = new venture(list.get(0).getNum(),list.get(0).getCompany(),
+			list.get(0).getAddr(), list.get(0).getCategory(),
+			list.get(0).getBusiness_name(),list.get(0).getProduct());	
+	
+	//정보처리 집합과 도소매 집합의 차집합 구하기 => addr기준
+	//정보처리 집합과 도소매 집합의 교집합 구하기 => addr기준
+	
+	
+	Set<String> addr_gyo = new HashSet<>();
+	Set<String> addr_cha = new HashSet<>();
+	
+	Iterator<venture> gp = sw.iterator(); //	
+	while(gp.hasNext()) {
+		venture temp = gp.next();
+		if(retail.contains(temp))
+			continue;
+			addr_cha.add(temp);
+	}
+	List<venture> cha_list = new ArrayList<>(addr_cha);
+	System.out.println(cha_list.size());
+	for(venture tmp : cha_list)
+		System.out.println(tmp);
+
+
+	Iterator<venture> gp1 = sw.iterator(); //	
+	while(gp1.hasNext()) {
+		venture temp = gp1.next();
+		if(retail.contains(temp))
+		addr_gyo.add(temp);
+	}
+	List<venture> cha_list = new ArrayList<>(addr_gyo);
+	System.out.println(cha_list.size());
+	for(venture tmp : cha_list)
+		System.out.println(tmp);
+	
+//	
+//	while(test.hasNext()) {//retail
+//		String name = test.next();
+//		if(name1.contains(name))//name1 내부에 name2 값이 있는가?
+//		gyo.add(name);
+//	}
+//	System.out.println(gyo);
+//
+//	test= name2.iterator();
+//	while(test.hasNext()) {// name2의 데이터를 name1에 있는지 비교하여 없으면 저장
+//		String name = test.next();
+//		if(name1.contains(name))//name1 내부에 name2 값이 있는가?
+//		continue;// name1 내부에 name2 값이 있다면 이를 제외한 나머지
+//		cha.add(name);
+//	}
+//	System.out.println(cha);
+//	
+//	
+	
+	
+	
+	if(a.equals(b))
+		System.out.println("같다");
+	else 
+		System.out.println("다르다");	
+	
+	
+	if(list.contains(a))
+		System.out.println("있다");
+	else
+		System.out.println("없다");
+
+// contains처럼 내장 클래스에서 비교가 필요한는 동작을 하는 것들은 equals 메소드 동작함 
+// a그룹에 b가 있냐? => a클래스의 멤버와 b를 비교 => equals가 있어야 동작
+// 비교로 해야하는 집합 표현시 equals 필수
+	
+	
+	
 	
 	
 //	차집합, 교집합, 합집합
@@ -93,5 +221,5 @@ public class main_cmd {
 	}
 	
 	
-	
-}
+*/
+
--- 오후/Java/demo/src/main/java/com/example/demo/mapper/EmpMapper.java
+++ 오후/Java/demo/src/main/java/com/example/demo/mapper/EmpMapper.java
@@ -26,8 +26,54 @@ public interface EmpMapper {
 	 * @Comment : Dept테이블 전체조회
 	 */
 	List<DeptVO>selectDept();
+	// 넘겨주고 받는 데이터가 많으면 객체 생성해서 넘겨줘야함
 	
+	
+	/**
+	 * @Since : 2022. 12. 1.
+	 * @Author : Imflower
+	 * @Return : int
+	 * @Comment : 사원추가
+	 */
 	int insertEmp(EmpVO vo);
+	
+
+	/**
+	 * @Since : 2022. 12. 1.
+	 * @Author : Imflower
+	 * @Return : int
+	 * @Comment : 사원삭제
+	 */
+	int deleteEmp(int empno);
+	// delete는 사원번호 한개만 없애는 것이기 때문에 객체 안만들어줘도 됨
+	// 대신 아이디랑 이름 똑같아야함
+	
+	/**
+	 * @Since : 2022. 12. 1.
+	 * @Author : Imflower
+	 * @Return : int
+	 * @Comment : 사원정보수정
+	 */
+	int updateEmp(EmpVO vo);
+	
+	
+	/**
+	 * @Since : 2022. 12. 1.
+	 * @Author : Imflower
+	 * @Return : int
+	 * @Comment : 부서추가
+	 */
+	int insertDept(DeptVO dv);
+	
+
+	/**
+	 * @Since : 2022. 12. 1.
+	 * @Author : Imflower
+	 * @Return : int
+	 * @Comment : 부서삭제
+	 */
+	int deleteDept(int deptno);
+
 }
 
 
