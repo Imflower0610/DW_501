202c3d661fb746e70f0b5434c6e67549caaceead
Imflower0610 <110653050+Imflower0610@users.noreply.github.com>
2022-12-15 14:04:08 +0900

    fix : jpa crud update

--- 오후/Java/member/src/main/java/com/dw/member/model/Member.java
+++ 오후/Java/member/src/main/java/com/dw/member/model/Member.java
@@ -5,10 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import lombok.Data;
 
 // vo기능 + 테이블 생성
 // 테이블이름은 소문자로
+@Data
 @Entity
 @Table(name ="dw_member")
 public class Member {
@@ -20,24 +25,13 @@ public class Member {
 	private String name;
 	@Column
 	private int age;
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
+//	@Column(name="dept_id")
+//	private int deptId;
+//	
+	// @JoinColumn => member테이블에 dept_id라는 컬럼(fk)생성
+	@ManyToOne
+	@JoinColumn(name="dept_id")
+	private Dept dept;
 	
 	
 }
